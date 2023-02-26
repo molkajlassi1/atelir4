c673b073a7bdce09f6fd136269ba2c3bf98080c9
molkajlassi1 <93016786+jlassimolka@users.noreply.github.com>
2023-02-26 20:30:31 +0100

    test

---
+++ sf-full-project/src/Entity/Student.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StudentRepository::class)
+ */
+class Student
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $NSC;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNSC(): ?string
+    {
+        return $this->NSC;
+    }
+
+    public function setNSC(string $NSC): self
+    {
+        $this->NSC = $NSC;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+}
